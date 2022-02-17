681dc977d7a54be0f7470d0bf7dcd9f32276705c
clownkill <alehnovich90@yandex.ru>
2022-02-17 09:36:37 +0300

    check for redirect

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-
+from requests import HTTPError
 
 def get_dvmn_logo():
     url = 'https://dvmn.org/filer/canonical/1542890876/16/'
@@ -19,16 +19,25 @@ def get_book(url, file_name):
         file.write(response.content)
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
+
+
 def download_books(counts):
     file_path = 'books/'
     url = f'http://tululu.org/txt.php'
     os.makedirs(file_path, exist_ok=True)
     for id in range(1, counts+1):
         params = {
-            'id': id
+            'id': id,
         }
         response = requests.get(url, params)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except HTTPError:
+            continue
         file_name = f'{file_path}/{id}.txt'
         with open(file_name, 'wb') as file:
             file.write(response.content)
