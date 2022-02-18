c36519d02fef7c2870d4fd31d291c7a12ba04c6c
clownkill <alehnovich90@yandex.ru>
2022-02-18 15:43:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ from bs4 import BeautifulSoup
 from requests import HTTPError
 
 
-def get_dvmn_logo():
-    url = 'https://dvmn.org/filer/canonical/1542890876/16/'
-    file_name = 'dvmn.svg'
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(file_name, 'wb') as file:
-        file.write(response.content)
-
-
 def get_book(url, file_name):
     response = requests.get(url)
     response.raise_for_status()
