1e00b673e46f2108bf3d131d73c566662d3d56e2
clownkill <alehnovich90@yandex.ru>
2022-03-12 11:19:25 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,27 +1,36 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('template.html')
 
-with open('json/books.json', 'r', encoding='utf-8') as file:
-    books_json = file.read()
+def on_reload():
+    template = env.get_template('template.html')
 
-books = json.loads(books_json).values()
+    with open('json/books.json', 'r', encoding='utf-8') as file:
+        books_json = file.read()
+    books = json.loads(books_json).values()
 
-rendered_page = template.render(books=books)
+    rendered_page = template.render(books=books)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 
+def main():
+    on_reload()
 
+    server = Server()
+    server.watch('template.html', on_reload)
+
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
