7b240952984a43c2f738c201bc675f5f04d6583f
clownkill <alehnovich90@yandex.ru>
2022-03-23 11:58:53 +0300

    added book name transliterate to parse category

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -7,6 +7,7 @@ import bs4
 import requests
 from bs4 import BeautifulSoup
 from requests import HTTPError
+from transliterate import get_translit_function
 
 from parse_tululu_book_page import save_book_text, download_image, parse_book_page, check_for_redirect
 
@@ -66,7 +67,11 @@ def get_book_from_pages(
                     save_response = requests.get(text_url, params=params)
                     save_response.raise_for_status()
                     book_title = parsed_book_informations['title']
-                    filename = f'{book_id}. {book_title}'
+
+                    translit_ru = get_translit_function('ru')
+                    book_title = translit_ru(book_title, reversed=True).replace(' ', '_')
+
+                    filename = f'{book_id}.{book_title}'
                     save_book_text(save_response, filename, dest_folder)
                     if not skip_image:
                         download_image(parsed_book_informations['image_url'], dest_folder)
